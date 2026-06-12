f3fedf89af8cd605968d0ad1dc23c71574479e24
Florwie <wiemann@match.uni-hannover.de>
2026-06-12 17:35:05 +0200

    object can only be spawned once

--- assembly_manager/assembly_manager/assembly_manager.py
+++ assembly_manager/assembly_manager/assembly_manager.py
@@ -149,6 +149,11 @@ class AssemblyManagerNode(Node):
         object_publish_success = False
         msg = None
 
+        if self.assembly_scene_analyzer.check_component_exists(SpawnRequest.obj_name):
+            msg = f"Component '{SpawnRequest.obj_name}' already exists in the scene. Spawning aborted."
+            self.logger.error(msg)
+            return False, msg
+
         SpawnRequest.translation.x = SpawnRequest.translation.x + self.SPAWN_COMPONENT_OFFSET_X
         SpawnRequest.translation.y = SpawnRequest.translation.y + self.SPAWN_COMPONENT_OFFSET_Y
         SpawnRequest.translation.z = SpawnRequest.translation.z + self.SPAWN_COMPONENT_OFFSET_Z
